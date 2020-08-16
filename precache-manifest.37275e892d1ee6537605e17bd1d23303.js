988e7e29d061355e44ab66ffc38232fe8e1feff6
mytabworks <mytabworks@gmail.com>
2020-08-16 10:47:20 +0800

    initial

---
+++ precache-manifest.37275e892d1ee6537605e17bd1d23303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12707eae450eb6726e07f780f51e448e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ba772133bc62d1401f1",
+    "url": "/static/css/main.edd009ab.chunk.css"
+  },
+  {
+    "revision": "c385d245c5527b041107",
+    "url": "/static/js/2.98017d68.chunk.js"
+  },
+  {
+    "revision": "706efe607e93e8ec7364e839dee20546",
+    "url": "/static/js/2.98017d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba772133bc62d1401f1",
+    "url": "/static/js/main.28f5c33f.chunk.js"
+  },
+  {
+    "revision": "ec2f0df67b0bd9732329",
+    "url": "/static/js/runtime-main.bd019e7a.js"
+  }
+]);
