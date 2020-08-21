5773074fe8d7d7b22e79136f82cd7f5fc06f8d06
mytabworks <mytabworks@gmail.com>
2020-08-21 10:25:53 +0800

    ppp

---
+++ resort-layout/precache-manifest.617b2566370f1251c7170dc33060b998.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd98b304ce2d5a1f3fc301cee651abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc3102d02b4b4c6636d",
+    "url": "/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "33c762519e9d7f52d577",
+    "url": "/static/css/main.c304fc28.chunk.css"
+  },
+  {
+    "revision": "3dc3102d02b4b4c6636d",
+    "url": "/static/js/2.ee843b53.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.ee843b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c762519e9d7f52d577",
+    "url": "/static/js/main.d07eeb00.chunk.js"
+  },
+  {
+    "revision": "9f94abe8ee74d818807a",
+    "url": "/static/js/runtime-main.cfbeaeda.js"
+  },
+  {
+    "revision": "317bb8fea3185574975a38006c695fc8",
+    "url": "/static/media/mytabworks.317bb8fe.eot"
+  },
+  {
+    "revision": "6df9e3b34f906dd818f3048b592e78d1",
+    "url": "/static/media/mytabworks.6df9e3b3.ttf"
+  },
+  {
+    "revision": "b7a9623a71f13d74e5196a579e228f29",
+    "url": "/static/media/mytabworks.b7a9623a.woff"
+  },
+  {
+    "revision": "cc0f676b9f45171bdf6c7210799f5abe",
+    "url": "/static/media/mytabworks.cc0f676b.svg"
+  }
+]);
